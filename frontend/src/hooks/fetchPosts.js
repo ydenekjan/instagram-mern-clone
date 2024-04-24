fd30142b9ee3f923798fa715d0a5bcac7d0b4c9f
realSwiftyy <ydenekjan@gmail.com>
2024-04-25 01:54:58 +0200

    fix: production url

--- frontend/src/hooks/fetchPosts.js
+++ frontend/src/hooks/fetchPosts.js
@@ -4,7 +4,7 @@ export const fetchPosts = async () => {
   try {
     const currentUser = JSON.parse(localStorage.getItem("currentUser"));
     return await axios.get(
-      `http://localhost:5555/posts/followed/${currentUser.username}`,
+      `https://ig-clone-backend.onrender.com/posts/followed/${currentUser.username}`,
     );
   } catch (err) {
     console.error("Error fetching posts: ", err.message);
