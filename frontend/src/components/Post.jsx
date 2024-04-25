5c7e922e72777b0cc3e6068ad3af48df8c9db695
realSwiftyy <ydenekjan@gmail.com>
2024-04-25 11:36:12 +0200

    feat: liked, saved clickable

--- frontend/src/components/Post.jsx
+++ frontend/src/components/Post.jsx
@@ -1,13 +1,19 @@
 import React, { useEffect, useState } from "react";
 import {
+  FaBookmark,
+  FaHeart,
   FaRegBookmark,
   FaRegComment,
   FaRegHeart,
   FaRegPaperPlane,
 } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import { IoAddCircle, IoAddCircleOutline } from "react-icons/io5";
 
 export const Post = (props) => {
   const [postData, setPostData] = useState({});
+  const [liked, setLiked] = useState(false);
+  const [saved, setSaved] = useState(false);
 
   useEffect(() => {
     setPostData(props.props);
@@ -31,10 +37,34 @@ export const Post = (props) => {
       )}
       <div className="p-3 flex flex-col gap-2">
         <div className="flex gap-6 w-full h-auto relative">
-          <FaRegHeart size={28} />
+          <button
+            onClick={() => {
+              setLiked(!liked);
+            }}
+            type="button"
+          >
+            {liked ? (
+              <FaHeart color={"red"} size={28} />
+            ) : (
+              <FaRegHeart size={28} />
+            )}
+          </button>
+
           <FaRegComment size={28} />
           <FaRegPaperPlane size={28} />
-          <FaRegBookmark className="absolute right-0" size={28} />
+          <button
+            className="absolute right-0"
+            onClick={() => {
+              setSaved(!saved);
+            }}
+            type="button"
+          >
+            {saved ? (
+              <FaBookmark color={"black"} size={28} />
+            ) : (
+              <FaRegBookmark size={28} />
+            )}
+          </button>
         </div>
 
         <div className="flex gap-2">
